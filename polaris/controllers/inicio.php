ac7744b013e6fde50beff2653d83bc8fd60026dd
Eduardo Villota <eduardouio7@gmail.com>
2014-04-22 23:18:58 -0500

    se adecuan las librerias para las pantallas

--- polaris/controllers/inicio.php
+++ polaris/controllers/inicio.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Inicio extends CI_Controller {
 
 	/**
 	 * Im prime la pantalla de inicio
